4629da0493822044c5fc868664d755ea9fa04d2e
MipzZz <maksimilian.2002@mail.ru>
2025-07-15 01:27:32 +0300

    - Переписал модели с использованием freezed

--- lib/src/home/domain/entity/crypto_coin.dart
+++ lib/src/home/domain/entity/crypto_coin.dart
@@ -2,23 +2,21 @@ import 'dart:math' as math;
 import 'dart:ui';
 
 import 'package:crypto_app/src/core/utils/typedef.dart';
-import 'package:flutter/foundation.dart' show immutable;
-import 'package:json_annotation/json_annotation.dart';
-
+import 'package:freezed_annotation/freezed_annotation.dart';
 part 'crypto_coin.g.dart';
+part 'crypto_coin.freezed.dart';
 
-@immutable
-@JsonSerializable()
-class CryptoCoin {
-  final String symbol;
-  @JsonKey(name: 'priceUsd')
-  final String price;
+@freezed
+abstract class CryptoCoin with _$CryptoCoin {
+  CryptoCoin._({Color? color}) : color = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withAlpha(50);
+
+  factory CryptoCoin({
+    required String symbol,
+    @JsonKey(name: 'priceUsd') required String price,
+  }) = _CryptoCoin;
+
+  @override
   final Color color;
-
-  CryptoCoin({
-    required this.symbol,
-    required this.price,
-  }) : color = Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withAlpha(50);
 
   factory CryptoCoin.fromJson(JsonMap json) => _$CryptoCoinFromJson(json);
 }
--- lib/src/home/domain/entity/crypto_response.dart
+++ lib/src/home/domain/entity/crypto_response.dart
@@ -1,14 +1,16 @@
-import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 import 'crypto_coin.dart';
 
 part 'crypto_response.g.dart';
+part 'crypto_response.freezed.dart';
 
-@JsonSerializable()
-class CryptoResponse {
-  @JsonKey(name: 'data')
-  List<CryptoCoin> coins;
-  CryptoResponse(this.coins);
+@freezed
+abstract class CryptoResponse with _$CryptoResponse {
+  const factory CryptoResponse({
+    @JsonKey(name: 'data')
+    required List<CryptoCoin> coins,
+  }) = _CryptoResponse;
 
   factory CryptoResponse.fromJson(Map<String, dynamic> json) => _$CryptoResponseFromJson(json);
 }
